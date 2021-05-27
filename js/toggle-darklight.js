1658f9f153b4c40427a153e25bcf11825de36cda
Adrakaris <huyijun007@gmail.com>
2021-05-27 20:32:22 +0100

    Redid stylesheets and js to be more developer friendly

--- js/toggle-darklight.js
+++ js/toggle-darklight.js
@@ -1,54 +1,77 @@
-// file wrangling
-var url = location.pathname.split("/").reverse();
-var backtrack = 0;
+// themes
 
-if (window.location.host == "") {
-    // locally hosted
-    var i = 0;
-    while (url[i] != "Website") {
-        backtrack++;
-        i++;
+var light = {
+    "--bg-col": "#ffffff",
+    "--bg-active": "#f1f2e9",
+    "--txt-col": "#000000",
+    "--band-col": "#a17667",
+    "--band-txt": "#f1f2e9",
+    "--pink": "#a17667",
+    "--blue": "#1f7282",
+    "--green": "#3a6342",
+    "--overlay": "#00000030",
+    "--tai-home": "url(\"images/taichuan_smaller_size.jpg\")",
+    "--tai-smol": "url(\"images/taichuan_smaller_size_squish.jpg\")",
+    "--filter": "brightness(.8) contrast(1.2)",
+    "--img-bg": "#0000"
+}
+
+var dark = {  
+    "--bg-col": "#2e2e2e",
+    "--bg-active": "#333333",
+    "--txt-col": "#eaeaea",
+    "--band-col": "#6e5046",
+    "--band-txt": "#f1f2e9",
+    "--pink": "#c7917f",
+    "--blue": "#1f7282",
+    "--green": "#3a6342",
+    "--overlay": "#00000030",
+    "--tai-home": "url(\"images/taichuan_dark.jpg\")",
+    "--tai-smol": "url(\"images/taichuan_dark_squish.jpg\")",
+    "--filter": "brightness(.8) contrast(1.2)",
+    "--img-bg": "#fff"
+}
+
+
+// code
+
+
+function setTheme(thema) {
+    for (const [k, v] of Object.entries(thema)) {
+        // console.log(k, v);
+        style.setProperty(k, v);
     }
-    backtrack--;
-} else {
-    // assume remotely hosted
-    backtrack = url.length - 2;
 }
 
-var toBack = "";
-var i;
-for (i = 0; i < backtrack; i++) {
-    toBack = toBack + "../"
-}
 
-var dark = toBack + "style/dark.css"
-var light = toBack + "style/style.css"
-
-console.log(url)
-
-// Select the button
-const btn = document.querySelector(".dark-light");
-// Select the stylesheet <link>
-const theme = document.querySelector("#theme-link");
-
+var style = document.documentElement.style
+// var cssRoot = styleSheet.querySelector(":root");  // document.querySelector("#theme-link").querySelector(":root");
+const toggle = document.querySelector(".dark-light");
 const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
-    theme.href = dark;
+
+var indicator;
+
+if (currentTheme == "dark") {  // seems redudnant but in case localStorage has none
+    indicator = "dark"
+    setTheme(dark);
 } else {
-    theme.href = light;
+    indicator = "light"
+    setTheme(light);
 }
 
-// Listen for a click on the button
-btn.addEventListener("click", function() {
-  // If the current URL contains "ligh-theme.css"
-  if (theme.getAttribute("href") == dark) {
-    // ... then switch it to "light.css"
-    theme.href = light;
-    localStorage.setItem("theme", "light");
-  // Otherwise...
-  } else {
-    // ... switch it to "dark.css"
-    theme.href = dark;
-    localStorage.setItem("theme", "dark");
-  }
+
+
+toggle.addEventListener("click", function() {
+    // we need to check if it's dark or light
+    if (indicator == "dark") {
+        indicator = "light";
+        localStorage.setItem("theme", "light");
+        setTheme(light);
+    } else {
+        indicator = "dark";
+        localStorage.setItem("theme", "dark");
+        setTheme(dark);
+    }
 });
+
+
