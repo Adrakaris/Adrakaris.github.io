683dd6cad947da4538a85f87823045913db579ba
Adrakaris <huyijun007@gmail.com>
2021-06-10 09:00:27 +0100

    Reshuffled 仙侠 pages and added to blog

---
+++ fan/pages-fan.js
@@ -0,0 +1,21 @@
+/*
+    {
+        "date": new Date(Date.UTC()),
+        "link": "",
+        "title": "",
+        "desc": "</p>",
+        "picture": "",
+        "tags": []
+    },
+*/
+
+var fanPages = [
+    {
+        "date": new Date(Date.UTC(2021, 6, 10)),
+        "link": "blog-cael/zunli/index.html",
+        "title": "◆ The Collapse of the Ancient Empire",
+        "desc": "</p>",
+        "picture": "",
+        "tags": []
+    },
+]
