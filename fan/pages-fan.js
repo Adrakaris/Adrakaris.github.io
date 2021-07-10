b8bc04498f6f2c66b906943416d9728e36b150dd
Adrakaris <huyijun007@gmail.com>
2021-07-10 13:03:46 +0000

    [1] Update links to not use the final .html and just point to folder - cleaner on web [2] Add a page on linux mint

--- fan/pages-fan.js
+++ fan/pages-fan.js
@@ -12,7 +12,7 @@
 var fanPages = [
     {
         "date": new Date(Date.UTC(2021, 6, 10)),
-        "link": "blog-cael/zunli/index.html",
+        "link": "blog-cael/zunli/",
         "title": "◆ The Collapse of the Ancient Empire",
         "desc": "In <i>Zunli</i> 245, the Yin dynasty came to an end. It had presided over the Ancient Empire for just over one millennia, yet due to famine and disaster, the rule of Yin finally shattered. Today, the Empire is cleaved in four, with two Emperors and two Kings, all in uneasy stalemate.</p>",
         "picture": "",
