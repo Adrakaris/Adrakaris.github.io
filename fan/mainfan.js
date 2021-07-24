337abe03301c4b4c5ad19c181e8658f5a627db0a
Adrakaris <huyijun007@gmail.com>
2021-07-24 20:08:11 +0000

    Reshuffle links, tweak index, add the recent blogs section to fan

---
+++ fan/mainfan.js
@@ -0,0 +1,40 @@
+// populate the mainpage with the most three recent blog items
+var populateDiv = document.querySelector("#fillblog")
+
+function createDiv(block) {
+    var div = document.createElement("div");
+
+    
+
+    var title = document.createElement("h2");
+    var title_a = document.createElement("a");
+    title_a.setAttribute("class", "text"); title_a.setAttribute("href", block["link"]);
+    title_a.setAttribute("style", "color: var(--green)")
+    title_a.appendChild(document.createTextNode(block["title"]));
+    title.appendChild(title_a);
+    div.appendChild(title);
+
+    /*if (block["picture"] != "") {
+        var picture = document.createElement("img");
+        picture.setAttribute("src", block["picture"]);
+        picture.setAttribute("style", "max-width: 40px; width: 100%; float:none;")
+        div.appendChild(picture)
+        // to fix formatting
+    }   */
+    
+
+    var date = block["date"];
+    var description = `<p><b><i>${date.getFullYear()}/${date.getMonth()}/${date.getDate()}</i></b>. ` + block["desc"];
+    div.innerHTML += description;
+    div.setAttribute("style", "overflow: hidden; margin-top: 5px; padding-bottom: 5px;")
+
+    return div;
+}
+
+function populatebox(array) {
+    array.forEach((block) => {
+        populateDiv.appendChild(createDiv(block));
+    });
+}
+
+populatebox(fanPages.slice(0, 3))
