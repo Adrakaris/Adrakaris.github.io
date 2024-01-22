388750fca3d96705acbea662d2f59c86b747cd6f
Yijun H <me@yijun.hu>
2024-01-22 23:42:20 +0000

    Refactor fantasy to be more self contained

--- fan/pages-fan.js
+++ fan/pages-fan.js
@@ -12,7 +12,7 @@
 var fanPages = [
     {
         "date": new Date(Date.UTC(2021, 7, 24)),
-        "link": "blog-cael/3dragons/",
+        "link": "fan/blog/3dragons/",
         "title": "Three Dragons Are An Army",
         "desc": "During the era of long conflict, the western mountain kingdom of Luzan decided to attack the State of Ma in Taichuan, seeking to take advantage of the lack of a united ruler, but their entire army was defeated by Ma's three sons. This extract tells that story, and of the idiom <i>Three Dragons are an Army</i>.</p>",
         "picture": "",
@@ -20,7 +20,7 @@ var fanPages = [
     },
     {
         "date": new Date(Date.UTC(2021, 6, 10)),
-        "link": "blog-cael/zunli/",
+        "link": "fan/blog/zunli/",
         "title": "The Collapse of the Ancient Empire",
         "desc": "In <i>Zunli</i> 245, the Yin dynasty came to an end. It had presided over the Ancient Empire for just over one millennia, yet due to famine and disaster, the rule of Yin finally shattered. Today, the Empire is cleaved in four, with two Emperors and two Kings, all in uneasy stalemate.</p>",
         "picture": "",
@@ -28,7 +28,7 @@ var fanPages = [
     },
     {
         "date": new Date(Date.UTC(2020, 1, 1)),
-        "link": "blog-cael/geni/",
+        "link": "fan/blog/geni/",
         "title": "Genesis",
         "desc": "\"Gení\", a creation epic in Ancient Draconic, originating from the Land of Sun and Twisted Flax, long, long ago. It has been told as the creation of the world and of the draconic race</p>",
         "picture": "",
@@ -36,7 +36,7 @@ var fanPages = [
     },
     {
         "date": new Date(Date.UTC(2020, 1, 1)),
-        "link": "blog-cael/lianglong/",
+        "link": "fan/blog/lianglong/",
         "title": "The Tale of Two Brothers",
         "desc": "\"Yelvrekharon Aiser\", a poem that talks about the Rain Gods. \"They say there lives a dragon god, atop the holy peak. None have seen him though, yet we believe and pray, for the rains are kind.\"</p>",
         "picture": "",
@@ -44,7 +44,7 @@ var fanPages = [
     },
     {
         "date": new Date(Date.UTC(2020, 1, 1)),
-        "link": "blog-cael/yirandastron/",
+        "link": "fan/blog/yirandastron/",
         "title": "Yirandastron Tor",
         "desc": "The history of Yirandastron Tor, also known as the Fire Mountain, the domain of House Astron ever since it was first conquered by Rivian.</p>",
         "picture": "",
@@ -52,7 +52,7 @@ var fanPages = [
     },
     {
         "date": new Date(Date.UTC(2020, 1, 1)),
-        "link": "blog-cael/coinage/",
+        "link": "fan/blog/coinage/",
         "title": "Coinage",
         "desc": "Different currencies and their uses, across the Material Plane.</p>",
         "picture": "",
